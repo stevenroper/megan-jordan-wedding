546889e05e4efa0ea4d83b7bf8051522b1e344fd
Steven Roper <steven.m.roper@gmail.com>
2021-11-11 23:32:24 -0700

    Adds fuzzy search on list of fake people.

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,43 @@ import {
   Flex,
   Heading,
   Input,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Text,
 } from '@chakra-ui/react';
+import faker from 'faker';
+import Fuse from 'fuse.js';
 
 import colors from './constants/colors';
 
+type Person = {
+  name: string;
+  table: number;
+};
+
 export const App = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
+  const [people] = React.useState<Person[]>(
+    new Array(100).fill('').map(() => ({
+      name: faker.name.findName(),
+      table: Math.round(Math.random() * 12),
+    }))
+  );
+  const [filteredPeople, setFilteredPeople] = React.useState<any[]>([]);
+  const fuzzySearch = new Fuse(people, {
+    distance: 50,
+    minMatchCharLength: 2,
+    threshold: 0.15,
+    keys: ['name'],
+  });
+  const handleOnChange = (term: string) => {
+    setFilteredPeople(fuzzySearch.search(term));
+    setSearchTerm(term);
+  };
 
   return (
     <Box height="100vh" bgGradient={`linear(to-b, white, ${colors.ivory})`}>
@@ -30,7 +61,7 @@ export const App = () => {
         <Heading textAlign="center" marginBottom="1rem" color={colors.green}>
           Find your table
         </Heading>
-        <Flex>
+        <Flex marginBottom="1rem">
           <Input
             name="searchTerm"
             value={searchTerm}
@@ -38,7 +69,7 @@ export const App = () => {
             focusBorderColor={colors.green}
             _placeholder={{ color: colors.green, fontWeight: '500' }}
             marginRight="1rem"
-            onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={(e) => handleOnChange(e.target.value)}
             autoFocus
           />
           <Button
@@ -50,6 +81,31 @@ export const App = () => {
             Clear
           </Button>
         </Flex>
+        <Box maxHeight="25rem" overflowY="scroll">
+          {searchTerm.length >= 2 && filteredPeople.length > 0 && (
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Table Number</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {filteredPeople.map(({ item: person }, index) => (
+                  <Tr key={index}>
+                    <Td>{person?.name}</Td>
+                    <Td>{person?.table}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          )}
+          {searchTerm.length >= 2 && filteredPeople.length === 0 && (
+            <Text textAlign="center">
+              Hmm, we can't find your name. Try again.
+            </Text>
+          )}
+        </Box>
       </Container>
     </Box>
   );
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,5 @@
 const colors = {
-  darkBlue: '#EAF2E3',
+  darkBlue: '#3A5683',
   lightBlue: '#6C91C2',
   green: '#54755A',
   gold: '#B7953B',
